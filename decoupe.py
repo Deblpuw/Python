c34ff094b5e68bf02d725a40956f1091a56c6606
Deblpuw <104028017+Deblpuw@users.noreply.github.com>
2022-10-25 13:43:52 +0200

    Add files via upload

---
+++ decoupe.py
@@ -0,0 +1,6 @@
+csvfile = open('test.csv', 'r').readlines()
+filename = 1
+for i in range(len(csvfile)):
+    if i % 1000 == 0:
+        open(str(filename) + '.csv', 'w+').writelines(csvfile[i:i+1000])
+        filename += 1
